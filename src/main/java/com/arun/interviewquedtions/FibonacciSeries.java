13091ed62a30f80cff7773bd7b7d36bee7b97398
arun789 <adwiti1975@gmail.com>
2016-12-10 12:46:52 -0700

    fibonacci series

---
+++ src/main/java/com/arun/interviewquedtions/FibonacciSeries.java
@@ -0,0 +1,28 @@
+package com.arun.interviewquedtions;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+
+	/* series with the below 0,1,1,2,3,5,8,13.... */
+
+	public static void main(String[] args) {
+		int firstXnumber = 0;
+		System.out.println("Give first X fibonacci series");
+		Scanner scan = new Scanner(System.in);
+		int till = scan.nextInt();
+		System.out.println();
+		int sum = 1;
+		int previousSum = 0;
+		System.out.print(previousSum + " " + sum + " ");
+		while (firstXnumber < till) {
+			int temp = sum;
+			sum = sum + previousSum;
+			previousSum = temp;
+			firstXnumber++;
+
+			System.out.print(sum + " ");
+		}
+		scan.close();
+	}
+}
