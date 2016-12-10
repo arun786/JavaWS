217c4adfb6e3aecf837eaf8509251be62fd22aeb
arun789 <adwiti1975@gmail.com>
2016-12-10 13:29:24 -0700

    changes to Bigdecimal

--- src/main/java/com/arun/interviewquedtions/BiDecimalExample.java
+++ src/main/java/com/arun/interviewquedtions/BiDecimalExample.java
@@ -3,25 +3,27 @@ package com.arun.interviewquedtions;
 import java.math.BigDecimal;
 
 public class BiDecimalExample {
-	
+
 	public static void main(String[] args) {
-		/*for financial calculation we should be using bigdecimal*/
+		/* for financial calculation we should be using bigdecimal */
 		double amount1 = 4.15;
 		double amount2 = 5.16;
-		
+
 		System.out.println("the difference between 2 will be " + (amount2 - amount1));
-		
-		/*o/p will be 
-		the difference between 2 will be 1.0099999999999998*/
-		
-		BigDecimal amount3 = new BigDecimal("4.15");
-		BigDecimal amount4 = new BigDecimal("5.16");
-		
+
+		/*
+		 * o/p will be the difference between 2 will be 1.0099999999999998
+		 */
+
+		BigDecimal amount3 = new BigDecimal("4.157");
+		BigDecimal amount4 = new BigDecimal("5.169");
+
 		System.out.println("the difference will be as " + amount4.subtract(amount3));
-		
-		/*o/p will be
-		 * the difference will be as 1.01, its better to use Bigdecimal
-		 * */ 
+
+		/*
+		 * o/p will be the difference will be as 1.01, its better to use
+		 * Bigdecimal
+		 */
 	}
 
 }
