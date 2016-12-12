863417496f41a339649bc2dd7705c94bf7cb5be8
arun789 <adwiti1975@gmail.com>
2016-12-12 01:26:49 -0700

    volatile keyword

--- src/main/java/com/arun/Thread/ToPreventCachingVolatileKeyWord.java
+++ src/main/java/com/arun/Thread/ToPreventCachingVolatileKeyWord.java
@@ -4,6 +4,13 @@ import java.util.Scanner;
 
 public class ToPreventCachingVolatileKeyWord {
 
+	/*
+	 * The Java volatile keyword is used to mark a Java variable as
+	 * "being stored in main memory". More precisely that means, that every read
+	 * of a volatile variable will be read from the computer's main memory, and
+	 * not from the CPU cache, and that every write to a volatile variable will
+	 * be written to main memory, and not just to the CPU cache.
+	 */
 	/* To prevent caching of variable Volatile keyword is used */
 	public static void main(String[] args) {
 		demo4 d1 = new demo4();
@@ -22,8 +29,8 @@ public class ToPreventCachingVolatileKeyWord {
 class demo4 extends Thread {
 
 	/*
-	 * runner is made volatile so that value of runner is not cached and the loop never
-	 * stops
+	 * runner is made volatile so that value of runner is not cached and the
+	 * loop never stops
 	 */
 	private volatile boolean runner = true;
 
