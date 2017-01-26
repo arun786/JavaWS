1730052213586183d0863728b41c9d555f367ffa
arun789 <adwiti1975@gmail.com>
2017-01-26 02:16:26 -0700

    Immutable class

---
+++ src/main/java/com/arun/interviewquedtions/ImmutableClass.java
@@ -0,0 +1,33 @@
+package com.arun.interviewquedtions;
+
+public final class ImmutableClass {
+
+	/*
+	 * Immutable classes are those class whose state cannot be changed. Any
+	 * modification on Immutable Object will result in creation of another
+	 * Immutable Object
+	 * 
+	 * ways of making a class final
+	 * 
+	 * 1. class is made final so that it can be inherited 2. All the variables
+	 * are final 3. No setters
+	 */
+
+	private final String id;
+	private final String name;
+	
+	public ImmutableClass(String id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
