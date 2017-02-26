b5a204a0b09313eb6caa0090aa70d7e5fb373f1c
arun789 <adwiti1975@gmail.com>
2017-02-26 11:22:36 -0700

    comparator interface

--- src/main/java/com/arun/comparatorExplained/JukeBoxObjectSort.java
+++ src/main/java/com/arun/comparatorExplained/JukeBoxObjectSort.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class JukeBoxObjectSort {
@@ -18,6 +19,29 @@ public class JukeBoxObjectSort {
 		System.out.println(jbs.lstSong);
 		Collections.sort(jbs.lstSong);
 		System.out.println(jbs.lstSong);
+
+		// Based on Rating
+		Collections.sort(jbs.lstSong, jbs.new SongBasedOnRating());
+		System.out.println(jbs.lstSong);
+		
+		//based on bpm
+		Collections.sort(jbs.lstSong, jbs.new SongBasedOnBPM());
+		System.out.println(jbs.lstSong);
+	}
+
+	class SongBasedOnRating implements Comparator<Songs> {
+
+		@Override
+		public int compare(Songs o1, Songs o2) {
+			return o1.getRating().compareTo(o2.getRating());
+		}
+	}
+
+	class SongBasedOnBPM implements Comparator<Songs> {
+		@Override
+		public int compare(Songs o1, Songs o2) {
+			return o1.getBpm().compareTo(o2.getBpm());
+		}
 	}
 
 	public void ReadAFile() {
@@ -36,7 +60,7 @@ public class JukeBoxObjectSort {
 			br.close();
 		} catch (IOException e) {
 			e.printStackTrace();
-		}finally{
+		} finally {
 			try {
 				br.close();
 			} catch (IOException e) {
@@ -45,3 +69,13 @@ public class JukeBoxObjectSort {
 		}
 	}
 }
+
+
+
+/*O/p
+
+[Songs [title=1, artist=kishore, rating=1, bpm=10], Songs [title=2, artist=Rafi, rating=2, bpm=11], Songs [title=3, artist=Lata, rating=5, bpm=1], Songs [title=4, artist=kk, rating=3, bpm=12]]
+[Songs [title=3, artist=Lata, rating=5, bpm=1], Songs [title=2, artist=Rafi, rating=2, bpm=11], Songs [title=1, artist=kishore, rating=1, bpm=10], Songs [title=4, artist=kk, rating=3, bpm=12]]
+[Songs [title=1, artist=kishore, rating=1, bpm=10], Songs [title=2, artist=Rafi, rating=2, bpm=11], Songs [title=4, artist=kk, rating=3, bpm=12], Songs [title=3, artist=Lata, rating=5, bpm=1]]
+[Songs [title=3, artist=Lata, rating=5, bpm=1], Songs [title=1, artist=kishore, rating=1, bpm=10], Songs [title=2, artist=Rafi, rating=2, bpm=11], Songs [title=4, artist=kk, rating=3, bpm=12]]
+*/
