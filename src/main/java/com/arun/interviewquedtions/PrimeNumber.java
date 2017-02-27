5a52ebfeae827b55c890e541e3ba99d6b493c894
arun789 <adwiti1975@gmail.com>
2017-02-27 01:20:18 -0700

    prime number

---
+++ src/main/java/com/arun/interviewquedtions/PrimeNumber.java
@@ -0,0 +1,26 @@
+package com.arun.interviewquedtions;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter a number...");
+		System.out.println(isPrime(scan.nextInt()));
+		scan.close();
+	}
+
+	public static boolean isPrime(Integer number) {
+		if (number % 2 == 0) {
+			return false;
+		}
+
+		for (int i = 3; i * i <= number; i = i + 2) {
+			if (number % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
